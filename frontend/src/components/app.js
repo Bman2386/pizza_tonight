b86ac59c091532bbfd8677ac2ca5b2324135d1be
Brendon <brendon.biagi@gmail.com>
2021-05-13 17:52:11 -0400

    save work

---
+++ frontend/src/components/app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { Switch, Route } from 'react-router-dom';
+
+const App = () => (
+    <div>
+        
+        <Switch>
+            
+        </Switch>
+    </div>
+);
+
+export default App;
---
+++ frontend/src/reducers/root.js
@@ -0,0 +1,12 @@
+import { combineReducers } from 'redux';
+import session from './session_reducer';
+import errors from './session_errors_reducer';
+
+
+
+const RootReducer = combineReducers({
+    session,
+    errors
+});
+
+export default RootReducer;
